d0d351b953c47cbe95f97274b013983e9f81ac67
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-06-19 20:56:12 -0500

    Added boostrap styles to table

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -3,35 +3,42 @@ import React from "react";
 function ResultList(props) {
   return (
 
-    <table>
-      <tbody>
+    <table className="table">
+    <thead className="thead-dark">
       <tr>
-        <th>Name</th>
-        <th>Picture</th>
-        <th>Address</th>
-        <th>Email</th>
-        <th>Age</th>
-        <th>Phone</th>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Picture</th>
+        <th scope="col">Address</th>
+        <th scope="col">Email</th>
+        <th scope="col">Age</th>
+        <th scope="col">Phone</th>
       </tr>
-        {props.results.map(result =>(
-      <tr key={result.login.uuid}>
-          <td>{result.name.first+" "+ result.name.last} </td>
-          <td><img alt={result.name.first+" "+ result.name.last +"profile image"} src={result.picture.thumbnail}/> </td>
-          <td>{result.location.street.number+" "+ result.location.street.name+"\n"+result.location.city+", "+result.location.country} </td>
-          <td>{result.email} </td>
-          <td>{result.dob.age} </td>
-          <td>{result.cell} </td>
+   </thead>
+      <tbody>
+        {props.results.map((result, i) =>(
+         <tr key={result.login.uuid}>
+            <th scope="row">{i+1} </th>
+            <td>{result.name.first+" "+ result.name.last} </td>
+            <td><img alt={result.name.first+" "+ result.name.last +"profile image"} src={result.picture.thumbnail}/> </td>
+            <td>{result.location.street.number+" "+ result.location.street.name+"\n"+result.location.city+", "+result.location.country} </td>
+            <td>{result.email} </td>
+            <td>{result.dob.age} </td>
+            <td>{result.cell} </td>
           </tr>
         ))}
      </tbody>
     </table>
-    // <ul className="list-group">
-    //   {props.results.map(result => (
-    //     <li className="list-group-item" key={result.id}>
-    //       <img alt={result.title} className="img-fluid" src={result.images.original.url} />
-    //     </li>
-    //   ))}
-    // </ul>
+    /* 
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+*/
   );
 }
 
